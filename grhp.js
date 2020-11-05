3fa0b62c9fe85f09dc4ec9b7ed868624417c1fb2
Jack Bister <jack@jackbister.com>
2020-11-05 17:58:47 +0100

    Initial commit

---
+++ grhp.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+var jsdom_1 = require("jsdom");
+var glob = require("glob");
+function main() {
+    if (process.argv.length < 2) {
+        throw new Error("Usage: grhp [-i includeGlob] <querySelector>");
+    }
+    var includeGlobIndex = process.argv.indexOf("-i");
+    var includeGlob;
+    if (includeGlobIndex !== -1) {
+        if (includeGlobIndex >= process.argv.length - 2) {
+            throw new Error("Must specify a glob pattern after -i");
+        }
+        includeGlob = process.argv[includeGlobIndex + 1];
+    }
+    var domQuery = process.argv[process.argv.length - 1];
+    var includedFiles = glob.sync(includeGlob, {});
+    includedFiles.forEach(function (file) {
+        fs.readFile(file, {}, function (err, data) {
+            var dom = new jsdom_1.JSDOM(data);
+            try {
+                var elements = dom.window.document.querySelectorAll(domQuery);
+                if (elements.length > 0) {
+                    console.log(file);
+                }
+            }
+            catch (e) {
+                console.error(e);
+            }
+        });
+    });
+}
+main();
+//# sourceMappingURL=grhp.js.map
---
+++ grhp.ts
@@ -0,0 +1,38 @@
+import * as fs from "fs";
+import { JSDOM } from "jsdom";
+import * as glob from "glob";
+
+function main() {
+  if (process.argv.length < 2) {
+    throw new Error("Usage: grhp [-i includeGlob] <querySelector>");
+  }
+
+  const includeGlobIndex = process.argv.indexOf("-i");
+  let includeGlob: string;
+  if (includeGlobIndex !== -1) {
+    if (includeGlobIndex >= process.argv.length - 2) {
+      throw new Error("Must specify a glob pattern after -i");
+    }
+    includeGlob = process.argv[includeGlobIndex + 1];
+  }
+
+  const domQuery = process.argv[process.argv.length - 1];
+
+  const includedFiles = glob.sync(includeGlob, {});
+
+  includedFiles.forEach((file) => {
+    fs.readFile(file, {}, (err, data) => {
+      const dom = new JSDOM(data);
+      try {
+        const elements = dom.window.document.querySelectorAll(domQuery);
+        if (elements.length > 0) {
+          console.log(file);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    });
+  });
+}
+
+main();
